5c419a789743eda1c9f6700ae262a3da24165b25
melonsound <ghost.134@hotmail.com>
2021-08-25 22:08:28 +0300

    fix: авторизация по токену

--- DocCatalog.Api.Auth/Startup.cs
+++ DocCatalog.Api.Auth/Startup.cs
@@ -1,6 +1,7 @@
 using DocCatalog.Api.Auth.Helpers;
 using DocCatalog.Api.Auth.Models;
 using DocCatalog.Api.Auth.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -8,9 +9,11 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace DocCatalog.Api.Auth
@@ -32,9 +35,29 @@ namespace DocCatalog.Api.Auth
 
             services.Configure<AuthOptions>(authOptionsConfiguration);
 
+            var auth = authOptionsConfiguration.Get<AuthOptions>();
+            var key = Encoding.ASCII.GetBytes(auth.Secret);
             services.AddDbContext<AccountContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
+
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
 
             // configure DI for application services
             services.AddScoped<IAccountService, AccountService>();
@@ -49,6 +72,14 @@ namespace DocCatalog.Api.Auth
             }
 
             app.UseRouting();
+
+            app.UseCors(x => x
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+
+            app.UseAuthentication();
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
