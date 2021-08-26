712cdbdb125fe3cf2c135880bbfb6228f6e3789a
melonsound <ghost.134@hotmail.com>
2021-08-26 11:06:39 +0300

    feat: миграция модели ахивного документа

---
+++ DocCatalog.Api.Auth/Migrations/ArchiveDocument/20210825220302_ArchiveDocumentMigration.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DocCatalog.Api.Auth.Migrations.ArchiveDocument
+{
+    public partial class ArchiveDocumentMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ArchiveDocuments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FileName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SenderCode = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    RecipientCode = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Year = table.Column<int>(type: "int", nullable: false),
+                    Month = table.Column<int>(type: "int", nullable: false),
+                    Version = table.Column<int>(type: "int", nullable: false),
+                    FileType = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FileSize = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FileCreationDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ArchiveDocuments", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ArchiveDocuments");
+        }
+    }
+}
