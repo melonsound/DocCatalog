0fe8e71e44c52f2bd8064ab1d3e38fa135448190
melonsound <ghost.134@hotmail.com>
2021-08-26 11:07:42 +0300

    feat: модель и контекст бд архивного документа

---
+++ DocCatalog.Api.Auth/Models/ArchiveDocument.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DocCatalog.Api.Auth.Models
+{
+    public class ArchiveDocument
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FileName { get; set; }
+        public string SenderCode { get; set; }
+        public string RecipientCode { get; set; }
+        public int Year { get; set; }
+        public int Month { get; set; }
+        public int Version { get; set; }
+        public string FileType { get; set; }
+        public string FileSize { get; set; }
+        public DateTime FileCreationDate { get; set; }
+    }
+}
---
+++ DocCatalog.Api.Auth/Models/ArchiveDocumentContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DocCatalog.Api.Auth.Models
+{
+    public class ArchiveDocumentContext : DbContext
+    {
+        public ArchiveDocumentContext(DbContextOptions<ArchiveDocumentContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<ArchiveDocument> ArchiveDocuments { get; set; }
+    }
+}
